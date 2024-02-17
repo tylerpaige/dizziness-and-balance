f5db21315f1952d0bd9238cedcf90f39bab23eb4
Tyler Paige <tylerclarkpaige@gmail.com>
2024-02-17 13:27:41 -0500

    Add Google Analytics

--- app/(landing-page)/layout.jsx
+++ app/(landing-page)/layout.jsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import clsx from "clsx";
 import "./globals.css";
 
@@ -9,9 +10,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={clsx("font-normal", "bg-black", "m-0")}>
-        {children}
-      </body>
+      <body className={clsx("font-normal", "bg-black", "m-0")}>{children}</body>
+      <GoogleAnalytics gaId="G-24GL3E5F1J" />
     </html>
   );
 }
